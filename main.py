b6feee5e05e020a696577a87b3606ab31a2250ca
Miguel Angel <miguelangelhh17@gmail.com>
2024-01-22 09:14:28 -0500

    Update CORS settings and separate TTS audio function into another file

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
+import os
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-import io
 from pydantic import BaseModel
-import os
-import azure.cognitiveservices.speech as speechsdk
-from dotenv import load_dotenv
-
-TTS_VOICE_NAME = "es-PE-CamilaNeural"
-AUDIO_FOLDER = "audios"
-
-load_dotenv()
-speech_key = os.environ.get("SPEECH_KEY")
-service_region = os.environ.get("SERVICE_REGION")
-
-if not speech_key or not service_region:
-    raise ValueError("Missing SPEECH_KEY or SERVICE_REGION")
-
-speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-speech_config.speech_synthesis_voice_name = TTS_VOICE_NAME
+from tts_generator import generate_wav, AUDIO_FOLDER
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://gys-chatbot.vercel.app/",
-        "https://chatbot-gys.vercel.app/",
+        "https://gys-chatbot.vercel.app",
+        "https://chatbot-gys.vercel.app",
         "http://localhost",
         "http://localhost:3000"],
     allow_credentials=True,
@@ -35,49 +19,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/audios", StaticFiles(directory=os.path.abspath(AUDIO_FOLDER), html=True), name="audios")
+app.mount("/{AUDIO_FOLDER}", StaticFiles(directory=os.path.abspath(AUDIO_FOLDER), html=True), name="{AUDIO_FOLDER}")
 
 class TextAndIdInput(BaseModel):
     id: str
     text: str
 
 @app.post("/tts")
-
-async def generate_wav(input_data: TextAndIdInput, request: Request):
-    os.makedirs(AUDIO_FOLDER, exist_ok=True)
-    output_file = os.path.join(AUDIO_FOLDER, f"{input_data.id}.wav")
-    audio_config = speechsdk.audio.AudioOutputConfig(filename=output_file)
-
-    text_input = input_data.text
-    escaped_text = text_input.replace('&', '&amp;')
-
-    text_template = """
-    <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
-        <voice name="{}">
-            <mstts:express-as style="customerservice" styledegree="1.3" role="OlderAdultFemale">
-                {}
-            </mstts:express-as>
-        </voice>
-    </speak>
-    """
-
-    text = text_template.format(TTS_VOICE_NAME, escaped_text)
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-
+async def tts_endpoint(input_data: TextAndIdInput, request: Request):
     try:
-        result = speech_synthesizer.speak_ssml_async(text).get()
+        audio_url = await generate_wav(input_data, request)
+        return {"audio_url": audio_url}
+    except HTTPException as e:
+        raise e
 
-        if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-            audio_data = result.audio_data
-            with open(output_file, "wb") as file:
-                file.write(audio_data)
-            audio_url = str(request.base_url.replace(scheme="http", path=f"/audios/{input_data.id}.wav"))
-            # print(f"Audio URL: {audio_url}")
-
-            return {"audio_url": audio_url}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
-    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
---
+++ tts_generator.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+import azure.cognitiveservices.speech as speechsdk
+from fastapi import HTTPException, Request
+from pydantic import BaseModel
+
+load_dotenv()
+
+AUDIO_FOLDER = "audios"
+TTS_VOICE_NAME = "es-PE-CamilaNeural"
+SPEECH_KEY = os.environ.get("SPEECH_KEY")
+SERVICE_REGION = os.environ.get("SERVICE_REGION")
+
+if not SPEECH_KEY or not SERVICE_REGION:
+    raise ValueError("Missing SPEECH_KEY or SERVICE_REGION")
+
+speech_config = speechsdk.SpeechConfig(subscription=SPEECH_KEY, region=SERVICE_REGION)
+speech_config.speech_synthesis_voice_name = TTS_VOICE_NAME
+
+async def generate_wav(input_data: BaseModel, request: Request):
+    os.makedirs(AUDIO_FOLDER, exist_ok=True)
+    output_file = os.path.join(AUDIO_FOLDER, f"{input_data.id}.wav")
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=output_file)
+
+    text_input = input_data.text
+    escaped_text = text_input.replace('&', '&amp;')
+
+    text_template = """
+    <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
+        <voice name="{}">
+            <mstts:express-as style="customerservice" styledegree="1.3" role="OlderAdultFemale">
+                {}
+            </mstts:express-as>
+        </voice>
+    </speak>
+    """
+
+    text = text_template.format(TTS_VOICE_NAME, escaped_text)
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+    try:
+        result = speech_synthesizer.speak_ssml_async(text).get()
+
+        if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+            audio_url = str(request.base_url.replace(scheme="http", path=f"/{AUDIO_FOLDER}/{input_data.id}.wav"))
+            return audio_url
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
