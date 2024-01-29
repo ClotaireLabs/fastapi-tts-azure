2567d6246f45604e61b4c993a6fe9d746852807a
Miguel Angel <miguelangelhh17@gmail.com>
2024-01-28 20:18:20 -0500

    refactor: speech synthesis module

--- tts_generator.py
+++ tts_generator.py
@@ -8,40 +8,52 @@ load_dotenv()
 
 AUDIO_FOLDER = "audios"
 TTS_VOICE_NAME = "es-PE-CamilaNeural"
-SPEECH_KEY = os.environ.get("SPEECH_KEY")
-SERVICE_REGION = os.environ.get("SERVICE_REGION")
 
-if not SPEECH_KEY or not SERVICE_REGION:
-    raise ValueError("Missing SPEECH_KEY or SERVICE_REGION")
+def setup_speech_config():
+    key = os.environ.get("SPEECH_KEY")
+    region = os.environ.get("SERVICE_REGION")
 
-speech_config = speechsdk.SpeechConfig(subscription=SPEECH_KEY, region=SERVICE_REGION)
-speech_config.speech_synthesis_voice_name = TTS_VOICE_NAME
+    if not key or not region:
+        raise ValueError("Missing SPEECH_KEY or SERVICE_REGION")
 
-async def generate_wav(input_data: BaseModel, request: Request):
-    output_file = os.path.join(AUDIO_FOLDER, f"{input_data.id}.wav")
-    audio_config = speechsdk.audio.AudioOutputConfig(filename=output_file)
+    config = speechsdk.SpeechConfig(subscription=key, region=region)
+    config.speech_synthesis_voice_name = TTS_VOICE_NAME
+    return config
 
-    text_input = input_data.text
-    escaped_text = text_input.replace('&', '&amp;')
+speech_config = setup_speech_config()
 
+def synthesize_speech(text, audio_config):
     text_template = """
-    <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
+    <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="es-PE">
         <voice name="{}">
-            <mstts:express-as style="customerservice" styledegree="1.3" role="OlderAdultFemale">
+            <mstts:express-as style="customerservice" styledegree="1" role="OlderAdultFemale">
                 {}
             </mstts:express-as>
         </voice>
     </speak>
     """
 
-    text = text_template.format(TTS_VOICE_NAME, escaped_text)
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    formatted_text = text_template.format(TTS_VOICE_NAME, text)
+    synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
     try:
-        result = speech_synthesizer.speak_ssml_async(text).get()
+        result = synthesizer.speak_ssml_async(formatted_text).get()
 
         if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-            audio_url = str(request.base_url.replace(scheme="http", path=f"/{AUDIO_FOLDER}/{input_data.id}.wav"))
-            return audio_url
+            return True
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+    return False
+
+async def generate_wav(input_data: BaseModel, request: Request):
+    output_file = os.path.join(AUDIO_FOLDER, f"{input_data.id}.wav")
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=output_file)
+
+    escaped_text = input_data.text.replace('&', '&amp;')
+
+    if synthesize_speech(escaped_text, audio_config):
+        audio_url = str(request.base_url.replace(scheme="http", path=f"/{AUDIO_FOLDER}/{input_data.id}.wav"))
+        return audio_url
+
+    raise HTTPException(status_code=500, detail="Failed to generate audio")
